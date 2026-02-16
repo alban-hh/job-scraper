90e2fffe4656e0a6883458e32eff5812eeb8127d
alban-hh <ashermadhi6@gmail.com>
2026-02-16 01:25:46 +0100

    shto config.py - URL-te e QKB, headers, fjalekyc, parametra kerkimi

---
+++ config.py
@@ -0,0 +1,39 @@
+URL_KERKIMI = "https://format.qkb.gov.al/kerko-per-subjekt/"
+URL_FLAMUJ = "https://format.qkb.gov.al/wp-content/themes/twentytwentyfive-child/modules/search/national-registry/subject/search-for-subject-get-red-flag.php"
+URL_DOKUMENT = "https://format.qkb.gov.al/wp-content/themes/twentytwentyfive-child/modules/search/national-registry/subject/search-for-subject-get-documents.php"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Origin": "https://format.qkb.gov.al",
+    "Referer": "https://format.qkb.gov.al/kerko-per-subjekt/",
+}
+
+FJALEKYC_TECH = ["internet", "software", "IT", "teknologji"]
+
+QYTETI_PARAZGJEDHJE = "tirane"
+
+QARKU_PARAZGJEDHJE = "tirane"
+
+VONESA_MIN = 1.0
+VONESA_MAX = 2.0
+
+FUSHAT_KERKIMIT = {
+    "orderColumn": "0",
+    "orderDir": "asc",
+    "nipt": "",
+    "emriISubjektit": "",
+    "emriTregtar": "",
+    "formeLigjore": "",
+    "pronesia": "",
+    "dataNga": "",
+    "dataNe": "",
+    "numriId": "",
+    "administrator": "",
+    "aksionerOrtak": "",
+    "sektoriIVeprimtarise": "",
+    "qarku": "",
+    "qyteti": "",
+    "adresa": "",
+}
